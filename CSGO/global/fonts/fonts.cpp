d3357c0557abc439c68d5d09060bf75b9eea0ea3
urosaurus <42598661+urosaurus@users.noreply.github.com>
2019-09-15 21:05:45 -0400

    all files

---
+++ CSGO/global/fonts/fonts.cpp
@@ -0,0 +1,10 @@
+#include "../../main.h"
+
+namespace f
+{
+	unsigned long arial_large;
+	unsigned long arial_small;
+	unsigned long arial_blur;
+	unsigned long arial;
+	unsigned long tahoma_large;
+}
